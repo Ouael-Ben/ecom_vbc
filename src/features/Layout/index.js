3497c13c602168fc41dde53d2004f60f2891e3fa
Ouael benchagra <obenchagra@vision-business-consulting.com>
2020-01-31 16:46:22 +0100

    first commit

---
+++ src/features/Layout/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+
+
+export default function Layout() {
+    return (
+        <Box m={1}>
+            hello
+        </Box>
+    )
+}
